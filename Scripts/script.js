e09784fe7d8d8a0bf39d4c51d5007732fb7d2051
AngelMValentin <angelvalentinusmc@gmail.com>
2024-11-04 17:27:15 -0800

    Class 1 work

---
+++ Scripts/script.js
@@ -0,0 +1,59 @@
+console.log("Script");
+
+let myName="Angel"; // declaration of var
+var mySize = 182
+var myAge = 99;
+
+console.log(myName);
+console.log(myAge,mySize);
+
+console.log("My name is " + myName);
+console.log("I am " + myAge + " years old and ");
+console.log("my size is " + mySize);
+
+// operations 
+
+let num1 = 10;
+let num2=2;
+
+let addition = num1+num2;
+
+console.log(" Addition is: " + addition)
+
+//subtraction mult div 
+
+subtraction = num1-num2;
+
+console.log(" Subtraction is: " + subtraction)
+
+multiplication = num1*num2;
+
+console.log(" Multiplication is: " + multiplication);
+
+division = num1/num2;
+
+console.log("Division is: " + division);
+
+const pi = 3.1416;
+let r = 3;
+//let a = pie*radius*radius;
+a=pi * (r*r);
+
+console.log("The area of a circle with a radius of " + r + " is " + a);
+
+let aVar = 20;
+
+let otherVar = 30;
+
+console.log(aVar,otherVar);
+
+aVar = 50;
+otherVar=60;
+
+console.log(aVar,otherVar); // 50 60
+
+const weekDays = 5;
+
+weekDays=10; // the constants can't change
+
+console.log(weekDays);
